8a77ccff068a5128fed9801462b1889064b51f38
Nickolay.NP <nickolay.np@gmail.com>
2023-05-06 21:10:35 +0300

    v.0.03

--- toNmon_test.go
+++ toNmon_test.go
@@ -2,26 +2,64 @@ package main
 
 import (
 	"bytes"
-	"io"
+	"strings"
 	"testing"
 )
 
 func Test_run(t *testing.T) {
-	type args struct {
-		sIn io.Reader
-	}
 	tests := []struct {
 		name     string
-		args     args
+		sInput   string
 		wantSOut string
 		wantErr  bool
 	}{
-		// TODO: Add test cases.
+		{"Test 1",
+			`20121015100000 1 2 3
+20121015100030 2 3 4
+20121015100100 1 2 3
+20121015100130 2 3 4
+20121015100200 1 2 3`,
+			`{
+"hostname": "dummy.rtp.raleigh.ibm.com",
+"whenPattern": "MM/dd/yyyy HH:mm:ss",
+"metadata": {
+	"mdata1": "value 1",
+	"mdata2": "value 2",
+	"mdata3": "completely arbitrary data goes here"
+},
+"types": [
+	{
+	"id": "type1",
+	"name": "type 1",
+	"fields": [ "field11", "field12", "field13" ]
+	}
+],
+"data": [
+	{ "when": "10/15/2012 10:00:00",
+	"type1": [ 1, 2, 3 ]
+	},
+	{ "when": "10/15/2012 10:00:30",
+	"type1": [ 2, 3 , 4 ]
+	},
+	{ "when": "10/15/2012 10:01:00",
+	"type1": [ 1, 2 , 3 ]
+	},
+	{ "when": "10/15/2012 10:01:30",
+	"type1": [ 2, 3 , 4 ]
+	},
+	{ "when" : "10/15/2012 10:02:00",
+	"type1": [ 1, 2 , 3 ],
+	}
+]
+}`,
+			false,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
+			sIn := strings.NewReader(tt.sInput)
 			sOut := &bytes.Buffer{}
-			if err := run(tt.args.sIn, sOut); (err != nil) != tt.wantErr {
+			if err := run(sIn, sOut); (err != nil) != tt.wantErr {
 				t.Errorf("run() error = %v, wantErr %v", err, tt.wantErr)
 				return
 			}
