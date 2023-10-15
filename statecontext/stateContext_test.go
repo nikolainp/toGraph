ed744e8d81e500dee5a14a8bc92e0718947c0b4f
Nickolay.NP <nickolay.np@gmail.com>
2023-10-15 12:16:39 +0300

    v.0.22

--- statecontext/stateContext_test.go
+++ statecontext/stateContext_test.go
@@ -2,19 +2,19 @@ package statecontext
 
 import (
 	"testing"
-//	"time"
+	"time"
 )
 
 func TestGo(t *testing.T) {
-	// InitState()
-	// t.Run("signal waiting", func(t *testing.T) {
-	// 	Go(func() {
-	// 		select {
-	// 		case <-Done():
-	// 		case <-time.After(5 * time.Second):
-	// 			t.Errorf("done not work: overslept")
-	// 		}
-	// 	})
-	// 	//syscall.Kill(syscall.Getpid(), syscall.SIGINT) // stop waiting signal
-	// })
+	InitState()
+	t.Run("signal waiting", func(t *testing.T) {
+		Go(func() {
+			select {
+			case <-Done():
+			case <-time.After(5 * time.Second):
+				t.Errorf("done not work: overslept")
+			}
+		})
+		//syscall.Kill(syscall.Getpid(), syscall.SIGINT) // stop waiting signal
+	})
 }
